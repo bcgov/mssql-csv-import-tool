8242a1b2648b9a9746710dbba9b982eeeee99b7d
Jonathan Longe <jlonge@corebox.net>
2021-07-15 15:58:35 -0700

    update import tool description

--- bin/mssql_import.py
+++ bin/mssql_import.py
@@ -5,8 +5,10 @@ from src.config import Config
 
 
 def main():
-    d = 'Clean and verify CSV file, bulk import to temporary table and then, '
-    d += 'merge temporary table with destination table.'
+    d = 'Clean and verify CSV file, import to a temporary table and '
+    d += 'merge the temporary table with destination table. Dates are assumed'
+    d += 'to be in a format with the day before the month. ie "28/02/2021"'
+    d += '(version: 0.18)'
     parser = argparse.ArgumentParser(description=d)
     parser.add_argument('-f', '--filename', required=True, help='path and filename of the CSV file for import')
     parser.add_argument('-t', '--table',
